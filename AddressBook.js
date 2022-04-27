a6bd26f3911e8d97d25cda91b8598eea8bd75922
Krunali <krunalilole29@gmail.com>
2022-04-27 21:15:46 +0530

    Adding UC-9 to view person

--- AddressBook.js
+++ AddressBook.js
@@ -183,6 +183,11 @@ function searchContactInCity(city,name,array){
     else
     return true;
 }
+function viewContactsByCity(city,array){
+    let contacts = array.filter(e=>e.city == city).map(e=>e.firstName+" "+e.lastName);
+    return contacts;
+}
+
 
 addContact("Krunali","Lole","Old Subedhar","Nagpur","Maharashtra",440024,9561272972,"Krunalilole01@gmail.com");
 addContact("Rohit","Lole","New Subedhar","Nagpur","Maharashtra",440026,7777979699,"rohitlole@outlook.com");
@@ -212,3 +217,9 @@ console.log("The person "+name+" is found in the city "+City);
 else
 console.log("The person "+name+" is not found in the city "+City);
 
+City = "Nagpur";
+let contacts = viewContactsByCity(City,addressBook);
+if(contacts.length>0)
+console.log("The people in the city "+City+" are :"+contacts);
+else
+console.log("No people found in the city");
